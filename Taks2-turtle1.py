c3a4164b688bdff80ab1d79ea541f2343bd75d23
FloatingJupiter <60292827+FloatingJupiter@users.noreply.github.com>
2021-01-13 07:01:42 +0300

    Add files via upload

---
+++ Taks2-turtle1.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.shape('turtle')
+turtle.speed(100)
+x = 0
+for x in range(60):
+  for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+  turtle.right(5)
---
+++ Task2-turtle2.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(100)
+scale = 0
+for i in range(60):
+    scale += 5
+    for x in range(4):
+        turtle.forward(25+scale)
+        turtle.right(90)
+    turtle.right(5)
