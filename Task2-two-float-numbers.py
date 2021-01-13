d512320183e497d027e1120842cb3ac9584ce1ff
FloatingJupiter <60292827+FloatingJupiter@users.noreply.github.com>
2021-01-13 06:47:07 +0300

    Add files via upload

---
+++ Task2-two-float-numbers.py
@@ -0,0 +1,10 @@
+#Script to add two floating numbers
+# Store two numbers from userinput
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two floating numbers
+sum = float(num1) + float(num2)
+
+# Display the sum 
+print(sum)
